d8eabbd61b8b50e31b04d10bcf2a1ed2daaf8186
Kyle Krull <kdkrull@gmail.com>
2025-01-30 14:20:05 -0600

    Add padding

---
+++ develop-in-swift/DevelopInSwift/Pages/OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomePage.swift
+//  DevelopInSwift
+//
+//  Created by Kyle Krull on 1/30/25.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 150, height: 150)
+                .foregroundStyle(.tint)
+            
+            Text("Welcome to the Develop in Swift App")
+                .border(.black, width: 1.5)
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.top)
+            
+            Text("Descriptive description here from the Department of Redundancy Department")
+                .border(.black, width: 1.5)
+                .font(.title2)
+        }
+        .border(.orange, width: 1.5)
+        .padding()
+        .border(.purple, width: 1.5)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
