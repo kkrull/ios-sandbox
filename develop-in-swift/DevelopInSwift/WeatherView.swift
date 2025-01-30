96d59656095b4cf332c2835c890a81a91acab909
Kyle Krull <kdkrull@gmail.com>
2025-01-30 11:02:02 -0600

    Embed in ScrollView

--- develop-in-swift/DevelopInSwift/WeatherView.swift
+++ develop-in-swift/DevelopInSwift/WeatherView.swift
@@ -2,22 +2,19 @@ import SwiftUI
 
 struct WeatherView: View {
     var body: some View {
-        VStack {
+        
+        ScrollView(.horizontal) {
             HStack {
                 DayForecast(day: "Monday", isRainy: false, high: 2, low: 1)
                     .padding()
                 DayForecast(day: "Tuesday", isRainy: true, high: 8, low: 4)
                     .padding()
-            }
-            HStack {
                 DayForecast(day: "Wednesday", isRainy: false, high: 32, low: 16)
                     .padding()
                 DayForecast(day: "Thursday", isRainy: true, high: 128, low: 64)
                     .padding()
                 DayForecast(day: "Friday", isRainy: false, high: -1, low: -2)
                     .padding()
-            }
-            HStack {
                 DayForecast(day: "Saturday", isRainy: true, high: -4, low: -8)
                     .padding()
                 DayForecast(day: "Sunday", isRainy: false, high: -16, low: -32)
