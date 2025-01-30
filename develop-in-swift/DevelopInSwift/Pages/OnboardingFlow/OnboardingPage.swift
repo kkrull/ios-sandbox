7dd2656411022b95d9271d51e00769460c4f8487
Kyle Krull <kdkrull@gmail.com>
2025-01-30 15:08:47 -0600

    Define color sets in assets

--- develop-in-swift/DevelopInSwift/Pages/OnboardingFlow/OnboardingPage.swift
+++ develop-in-swift/DevelopInSwift/Pages/OnboardingFlow/OnboardingPage.swift
@@ -1,11 +1,18 @@
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct OnboardingPage: View {
     var body: some View {
         TabView {
             WelcomeTab()
             FeaturesTab()
         }
+        .background(Gradient(colors: gradientColors))
+        .foregroundStyle(.white)
         .tabViewStyle(.page)
     }
 }
