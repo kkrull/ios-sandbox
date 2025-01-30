00aa190c942735e250f5e5c33d8fbd42d29bfdcc
Kyle Krull <kdkrull@gmail.com>
2025-01-30 14:41:56 -0600

    Add more feature views

---
+++ develop-in-swift/DevelopInSwift/Pages/OnboardingFlow/FeatureCard.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct FeatureCard: View {
+    let iconName: String
+    let description: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: iconName)
+                .font(.largeTitle)
+            
+            Text(description)
+        }
+        .padding()
+        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
+        .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    FeatureCard(
+        iconName: "hands.sparkles",
+        description: "A multiline description that says in way to many words what this feature might do.  But how can a feature be useful to a user, when the developers don't know themselves?"
+    )
+}
---
+++ develop-in-swift/DevelopInSwift/Pages/OnboardingFlow/FeaturesPage.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct FeaturesPage: View {
+    var body: some View {
+        VStack {
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FeaturesPage()
+}
